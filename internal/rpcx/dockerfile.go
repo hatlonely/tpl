4578a36fd61d3d90b9f4ee0727a0e641ab1c4140
hatlonely <hatlonely@gmail.com>
2022-03-11 23:09:13 +0800

    update

--- internal/rpcx/dockerfile.go
+++ internal/rpcx/dockerfile.go
@@ -1,1 +1,17 @@
 package rpcx
+
+var dockerfile = `
+FROM golang:1.16 AS build
+
+COPY . /go/src/
+WORKDIR /go/src/
+RUN make build
+
+FROM centos:centos7
+RUN ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+RUN echo "Asia/Shanghai" >> /etc/timezone
+
+COPY --from=build /go/src/build /work/{{ .Name }}
+WORKDIR /work/{{ .Name }}
+CMD [ "bin/{{ .Name }}", "-c", "config/base.json" ]
+`
