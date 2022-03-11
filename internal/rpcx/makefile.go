3e7d3369846761291c20eefa1c867c8284974929
hatlonely <hatlonely@gmail.com>
2022-03-11 23:00:47 +0800

    update

---
+++ internal/rpcx/makefile.go
@@ -0,0 +1,5 @@
+package rpcx
+
+var makefile = `
+include .tpl.mk
+`
--- internal/rpcx/rpcx.go
+++ internal/rpcx/rpcx.go
@@ -24,34 +24,45 @@ func NewTemplateWithOptions(options *Options) (*Template, error) {
 		return nil, errors.Wrap(err, "template.New tplMk failed")
 	}
 
+	makefile, err := template.New("").Parse(makefile)
+	if err != nil {
+		return nil, errors.Wrap(err, "template.New makefile failed")
+	}
+
 	return &Template{
-		options: options,
-		TplMk:   tplMk,
+		options:  options,
+		TplMk:    tplMk,
+		Makefile: makefile,
 	}, nil
 }
 
 type Template struct {
-	options *Options
-	TplMk   *template.Template
+	options  *Options
+	TplMk    *template.Template
+	Makefile *template.Template
 }
 
 func (t *Template) Render(prefix string) error {
-	if err := render(t.TplMk, t.options, prefix, ".tpl.mk"); err != nil {
+	if err := render(t.TplMk, t.options, fmt.Sprintf("%v/.tpl.mk", prefix)); err != nil {
 		return errors.Wrap(err, "render tplMK failed")
+	}
+
+	if err := render(t.TplMk, t.options, fmt.Sprintf("%v/Makefile", prefix)); err != nil {
+		return errors.Wrap(err, "render Makefile failed")
 	}
 
 	return nil
 }
 
-func render(tpl *template.Template, options *Options, prefix string, out string) error {
-	prefix, err := filepath.Abs(prefix)
+func render(tpl *template.Template, options *Options, out string) error {
+	abs, err := filepath.Abs(out)
 	if err != nil {
 		return errors.Wrap(err, "filepath.Abs failed")
 	}
-	if err := os.MkdirAll(prefix, 0755); err != nil {
+	if err := os.MkdirAll(filepath.Dir(abs), 0755); err != nil {
 		return errors.Wrap(err, "os.MkdirAll failed")
 	}
-	fp, err := os.Create(fmt.Sprintf("%v/%v", prefix, out))
+	fp, err := os.Create(abs)
 	if err != nil {
 		return errors.Wrap(err, "os.Open failed")
 	}
