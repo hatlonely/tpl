28ef6d7997deabe97bf8593a384af3c1ec7914d8
hatlonely <hatlonely@gmail.com>
2022-03-12 15:06:14 +0800

    update

--- internal/rpcx/readme_md.go
+++ internal/rpcx/readme_md.go
@@ -3,6 +3,22 @@ package rpcx
 var readmeMd = `
 # {{ .Name }}
 
+## 初始化项目
+
+1. 生成 proto 代码
+
+` + "```" + `
+make codegen
+` + "```" + `
+
+2. 初始化
+
+` + "```" + `
+go mod init {{ .Package }}
+go mod tidy
+go mod vendor
+` + "```" + `
+
 ## make 命令
 
 1. 代码生成 ` + "`" + `make codegen` + "`" + `
